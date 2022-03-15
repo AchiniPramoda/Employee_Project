d78923499d07cc9791d1da4024362d86d0f85f39
apm <achinichandrasena38@gmail.com>
2022-03-15 13:44:27 -0700

    add user.js

--- employee_backend/models/user.js
+++ employee_backend/models/user.js
@@ -21,8 +21,7 @@ const User = mongoose.model("user", userSchema);
 
 const validate = (data) => {
 	const schema = Joi.object({
-		firstName: Joi.string().required().label("First Name"),
-		lastName: Joi.string().required().label("Last Name"),
+		
 		email: Joi.string().email().required().label("Email"),
 		password: passwordComplexity().required().label("Password"),
 	});
