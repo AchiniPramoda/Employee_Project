cc8a17a89226537325ea1e9ee49614c43f7a9b72
apm <achinichandrasena38@gmail.com>
2022-03-15 11:23:32 -0700

    create the user login

--- employee_backend/models/user.js
+++ employee_backend/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
 
 const userSchema = new mongoose.Schema({
 	firstName: { type: String, required: true },
@@ -16,6 +17,16 @@ userSchema.methods.generateAuthToken = function () {
 	return token;
 };
 
+const User = mongoose.model("user", userSchema);
 
+const validate = (data) => {
+	const schema = Joi.object({
+		firstName: Joi.string().required().label("First Name"),
+		lastName: Joi.string().required().label("Last Name"),
+		email: Joi.string().email().required().label("Email"),
+		password: passwordComplexity().required().label("Password"),
+	});
+	return schema.validate(data);
+};
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = { User, validate };
